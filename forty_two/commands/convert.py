405f677fbfd21260a5f1538e823a092d6f33e494
Craig N. Caroon <craig.caroon@cengage.com>
2025-08-22 15:12:00 -0400

    * Added meters, and kilometers * Changed `convert` to have sub-commands * Renamed old `convert` to `length` and renamed a few other things appropriately

--- forty_two/commands/convert.py
+++ forty_two/commands/convert.py
@@ -4,23 +4,30 @@ Convert - Unit Conversion
 import click
 import re
 
-CONVERSION_MAP = {
-    "units":      ("mm", "cm", "in", "ft", "yd", "mi"),
-    "conv_amt":   ( 1,    10,   2.54, 12,   3,    1760)
+@click.group
+def convert():
+    """ Unit Conversion """
+    pass
+
+# Each successive unit is defined in the previous unit
+# Example: 1yd is 3 feet, thus "yd" conv_amt is "3"
+#          1ft is 12 inches, thus "ft" conv_amt is "12"
+#          Etc, down to the base unit of 1 mm.
+LENGTH_UNIT_MAP = {
+    "units":      ("mm", "cm", "in", "ft", "yd", "m",                    "km", "mi"),
+    "conv_amt":   ( 1,    10,   2.54, 12,   3,    1.09361329833770778653, 1000, 1.609344)
 }
 
 # convert
-@click.command()
+@convert.command()
 @click.argument("amount")
 @click.argument("dest_units")
-def convert(amount, dest_units):
+def length(amount, dest_units):
     """
-    Convert between measurements: amount -> dest_units
-
-    Known Units: mm, cm, in, ft, yd, mi
+    Convert between lengths: amount -> dest_units
 
     # Convert 1 Inch to Millimeters
-    >>> forty-two convert 1in mm
+    >>> forty-two convert length 1in mm
     1.0in => 25.4mm
     """
     matches = re.match(r"(\d+(\.\d+)?)(.*)", amount)
@@ -28,36 +35,35 @@ def convert(amount, dest_units):
     input_units = matches.group(3)
 
     try:
-        result = __convert_imperial(input_value, input_units, dest_units)
+        result = __convert_length(input_value, input_units, dest_units)
         print(f"{input_value}{input_units} => {result}{dest_units}")
     except ValueError as err:
         print(err)
+        print(f"Known Units: {LENGTH_UNIT_MAP['units']}")
 
 
-# "units":      ("mm", "cm", "in", "ft", "yd", "mi"),
-# "conv_amt":   ( 1,    10,   2.54, 12,   3,    1760)
-def __convert_imperial(amount, src_unit, dest_unit):
+def __convert_length(amount, src_unit, dest_unit):
     """
-    5280, ft, mi
+    Convert between length units using recursion.
     """
     result = amount
     try:
-        src_unit_idx = CONVERSION_MAP["units"].index(src_unit)
-        dest_unit_idx = CONVERSION_MAP["units"].index(dest_unit)
+        src_unit_idx = LENGTH_UNIT_MAP["units"].index(src_unit)
+        dest_unit_idx = LENGTH_UNIT_MAP["units"].index(dest_unit)
     except ValueError:
         raise ValueError(f"Don't know how to convert from '{src_unit}' to '{dest_unit}'")
 
     if src_unit_idx < dest_unit_idx:
         # Convert Up
-        conv_amt = CONVERSION_MAP["conv_amt"][src_unit_idx + 1]
-        new_unit = CONVERSION_MAP["units"][src_unit_idx + 1]
+        conv_amt = LENGTH_UNIT_MAP["conv_amt"][src_unit_idx + 1]
+        new_unit = LENGTH_UNIT_MAP["units"][src_unit_idx + 1]
         new_amt = amount / conv_amt
-        result = __convert_imperial(new_amt, new_unit, dest_unit)
+        result = __convert_length(new_amt, new_unit, dest_unit)
     elif src_unit_idx > dest_unit_idx:
         # Convert Down
-        conv_amt = CONVERSION_MAP["conv_amt"][src_unit_idx]
-        new_unit = CONVERSION_MAP["units"][src_unit_idx - 1]
+        conv_amt = LENGTH_UNIT_MAP["conv_amt"][src_unit_idx]
+        new_unit = LENGTH_UNIT_MAP["units"][src_unit_idx - 1]
         new_amt = amount * conv_amt
-        result = __convert_imperial(new_amt, new_unit, dest_unit)
+        result = __convert_length(new_amt, new_unit, dest_unit)
 
     return result
